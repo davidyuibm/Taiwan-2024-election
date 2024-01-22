3eb4efd792c94d908fc66ea9a35b7aef43999de9
davidyuibm <65706081+davidyuibm@users.noreply.github.com>
2024-01-22 08:15:22 +0800

    Initial commit

---
+++ data/election.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+
+df = pd.read_excel("Taipei.xlsx")
+
+print(df.loc[922:924])
+'''   
+print(df[922:925])
+print(df.get('南港區'))
+print('test', df.keys)
+print('test 99 ', df.values) 
+'''
